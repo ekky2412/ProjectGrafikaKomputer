b822b12f8ae9dab99501063677648eefa83bf98d
ekky2412 <ekky.raharjo@gmail.com>
2021-06-18 08:20:03 +0700

    add rotate dan fix array titik

--- project.py
+++ project.py
@@ -1,12 +1,19 @@
 from tkinter import *
 import tkinter
 import numpy as np
+import math
 from tkinter import colorchooser
 from tkinter.colorchooser import askcolor
 
+from numpy.lib.function_base import append
+
 class Paint(object):
 
+    WIDTH = 600
+    HEIGHT = 600
+    center = [WIDTH/2,HEIGHT/2]
     DEFAULT_COLOR = 'black'
+    DEFAULT_COLOR_OUTLINE = 'black'
     CLICKED_BUTTON = ''
     titik = []
     arah_translasi = ["Kiri Atas","Atas","Kanan Atas","Kanan","Kanan Bawah","Bawah","Kiri Bawah","Kiri"]
@@ -23,7 +30,7 @@ class Paint(object):
         icon_segitiga = icon_segitiga.subsample(3,3)
         
         #setting ukuran canvas sebesar 600px x 600px
-        self.c = Canvas(self.root,bg='white',width=600,height=600)
+        self.c = Canvas(self.root,bg='white',width=self.WIDTH,height=self.HEIGHT)
         self.c.grid(row=0)
 
         #Tombol Shape
@@ -37,12 +44,17 @@ class Paint(object):
         self.segitiga_button.grid(row=1,column=0,sticky=SW,padx=30)
 
         #Tombol Warna
-        self.pick_color_button = Button(self.root, text="Pilih Warna",command=self.pick_color)
+        self.pick_color_button = Button(self.root, text="Warna Fill",command=self.pick_color)
         self.pick_color_button.grid(row=0,column=1,sticky=N,padx=30)
         
         self.pick_color_hasil = Button(self.root,background=self.DEFAULT_COLOR)
         self.pick_color_hasil.grid(row=0,column=1,sticky=N,pady=30,ipadx=10)
 
+        self.pick_color_outline_button = Button(self.root,text="Warna Outline",command=self.pick_color_outline)
+        self.pick_color_outline_button.grid(row=0,column=1,sticky=N,padx=50,pady=60)
+
+        self.pick_color_outline_hasil = Button(self.root,background=self.DEFAULT_COLOR)
+        self.pick_color_outline_hasil.grid(row=0,column=1,sticky=N,pady=90,ipadx=10)
         #Tombol Translasi
         self.arah_button = StringVar(self.root)
         self.arah_button.set(self.arah_translasi[0])
@@ -55,6 +67,13 @@ class Paint(object):
 
         self.arah_submit = Button(self.root, text="Apply",command=self.translasi)
         self.arah_submit.grid(column=1,row=0,pady=250,sticky=S)
+
+        #Tombol Rotasi
+        self.rotasi_button = Button(self.root, text="Rotate",command=self.rotasi)
+        self.rotasi_button.grid(column=0,row=1,sticky=NE,padx=300,pady=10)
+
+        self.rotasi_value = Spinbox(self.root,width=5,from_=-100,to=100,increment=5)
+        self.rotasi_value.grid(column=0,padx=240,row=1,sticky=NE,pady=13)
 
         #Tombol Output Koordinat
         self.print_titik_kotak = Text(self.root, height=10, width=50)
@@ -78,10 +97,10 @@ class Paint(object):
 
         #variabel titik di cek, jika tidak ada isinya, maka menggunakan variabel yang ada
         if(titik == None):
-            self.titik = [200,200,400,400]
+            self.titik = [[200,200],[400,400]]
         else:
             self.titik = titik
-        self.c.create_oval(self.titik,fill= self.DEFAULT_COLOR,outline=self.DEFAULT_COLOR,width=2)
+        self.c.create_oval(self.titik,fill= self.DEFAULT_COLOR,outline=self.DEFAULT_COLOR_OUTLINE,width=2)
         self.CLICKED_BUTTON = "oval"
         self.print_titik()
 
@@ -90,10 +109,10 @@ class Paint(object):
         #(x1,y1,x2,y2)
         self.c.delete("all")
         if(titik == None):
-            self.titik = [200,200,400,400]
+            self.titik = [[200,200],[200,400],[400,400],[400,200],]
         else:
             self.titik = titik
-        self.c.create_rectangle(self.titik,fill=self.DEFAULT_COLOR,outline=self.DEFAULT_COLOR,width=2)
+        self.c.create_polygon(self.titik,fill=self.DEFAULT_COLOR,outline=self.DEFAULT_COLOR_OUTLINE,width=2)
         self.CLICKED_BUTTON = "kotak"
         self.print_titik()
 
@@ -105,10 +124,10 @@ class Paint(object):
         # y = 0 atas, y = 10 kebawah
         self.c.delete("all")
         if(titik == None):
-            self.titik = [200,400,400,400,300,200]
+            self.titik = [[200,400],[400,400],[300,200]]
         else:
             self.titik = titik
-        self.c.create_polygon(self.titik, fill=self.DEFAULT_COLOR,outline=self.DEFAULT_COLOR)
+        self.c.create_polygon(self.titik, fill=self.DEFAULT_COLOR,outline=self.DEFAULT_COLOR_OUTLINE)
         self.CLICKED_BUTTON = "segitiga"
         self.print_titik()
 
@@ -117,15 +136,20 @@ class Paint(object):
         self.DEFAULT_COLOR = colorchooser.askcolor(title = "Choose color")[1]
         self.onCLick(self.titik)
 
+    def pick_color_outline(self):
+        self.DEFAULT_COLOR_OUTLINE = colorchooser.askcolor(title = "Choose color")[1]
+        self.onCLick(self.titik)
+
 # Untuk menyimpan button apa yang telah ditekan antara bangun datar
     def onCLick(self,titik):
         if self.CLICKED_BUTTON == 'oval':
-            self.c.create_oval(titik,fill= self.DEFAULT_COLOR,width=2,outline=self.DEFAULT_COLOR)
+            self.c.create_oval(titik,fill= self.DEFAULT_COLOR,width=2,outline=self.DEFAULT_COLOR_OUTLINE)
         elif self.CLICKED_BUTTON == 'kotak':
-            self.c.create_rectangle(titik,fill=self.DEFAULT_COLOR,outline=self.DEFAULT_COLOR,width=2)
+            self.c.create_polygon(titik,fill=self.DEFAULT_COLOR,outline=self.DEFAULT_COLOR_OUTLINE,width=2)
         elif self.CLICKED_BUTTON == 'segitiga':
-            self.c.create_polygon(titik, fill=self.DEFAULT_COLOR,outline=self.DEFAULT_COLOR)
+            self.c.create_polygon(titik, fill=self.DEFAULT_COLOR,outline=self.DEFAULT_COLOR_OUTLINE)
         self.pick_color_hasil.configure(bg=self.DEFAULT_COLOR)
+        self.pick_color_outline_hasil.configure(bg=self.DEFAULT_COLOR_OUTLINE)
 
 # Untuk menulis output koordinat
     def print_titik(self):
@@ -133,6 +157,9 @@ class Paint(object):
         self.print_titik_kotak.insert(END,self.titik)
 
 # Fungsi pergeseran
+# 0 dalam koordinat x berarti kiri,
+# 0 dalam koordinat y berarti atas
+
     def translasi(self):
         if(self.arah_button.get() == "Kiri"):
             titik = []
@@ -141,10 +168,13 @@ class Paint(object):
             # index genap = koordinat x
             # index ganjil = koordinat y
             for i in range(0, len(self.titik)):
-                if i%2 == 0:
-                    titik.append(self.titik[i] - int(self.arah_value.get()))
-                else:
-                    titik.append(self.titik[i])
+                var_titik =[]
+                for j in range(0,len(self.titik[i])):
+                    if j%2 == 0:
+                        var_titik.append(self.titik[i][j] - int(self.arah_value.get()))
+                    else:
+                        var_titik.append(self.titik[i][j])
+                titik.append(var_titik)        
             
             # Untuk mengaplikasikan pergeseran sesuai bentuk
             if(self.CLICKED_BUTTON == "oval"):
@@ -155,8 +185,13 @@ class Paint(object):
                 self.use_segitiga(titik)
 
         elif(self.arah_button.get() == "Kiri Atas"):
-            titik =[x - int(self.arah_value.get()) for x in self.titik]
-            
+            titik =[]
+            for i in range(0,len(self.titik)):
+                var_titik = []
+                for j in range(0,len(self.titik[i])):
+                    var_titik.append(self.titik[i][j] - int(self.arah_value.get()))  
+                titik.append(var_titik)
+
             if(self.CLICKED_BUTTON == "oval"):
                 self.use_oval(titik)
             elif(self.CLICKED_BUTTON == "kotak"):
@@ -164,26 +199,145 @@ class Paint(object):
             elif(self.CLICKED_BUTTON == "segitiga"):    
                 self.use_segitiga(titik)
 
-# Next to do :
-# Bagian Atas hingga Kiri Bawah
         elif(self.arah_button.get() == "Atas"):
-            self.use_oval(np.subtract(self.titik,self.arah_value.get()))
+            titik = []
+            
+            for i in range(0, len(self.titik)):
+                var_titik =[]
+                for j in range(0,len(self.titik[i])):
+                    if j%2 == 1:
+                        var_titik.append(self.titik[i][j] - int(self.arah_value.get()))
+                    else:
+                        var_titik.append(self.titik[i][j])
+                titik.append(var_titik)
+            
+            # Untuk mengaplikasikan pergeseran sesuai bentuk
+            if(self.CLICKED_BUTTON == "oval"):
+                self.use_oval(titik)
+            elif(self.CLICKED_BUTTON == "kotak"):
+                self.use_kotak(titik)    
+            elif(self.CLICKED_BUTTON == "segitiga"):    
+                self.use_segitiga(titik)
+
         elif(self.arah_button.get() == "Kanan Atas"):
-            self.use_oval(np.subtract(self.titik,self.arah_value.get())) 
+            titik = []
+            for i in range(0, len(self.titik)):
+                var_titik =[]
+                for j in range(0,len(self.titik[i])):
+                    if j%2 == 1:
+                        var_titik.append(self.titik[i][j] - int(self.arah_value.get()))
+                    elif j%2 == 0:
+                        var_titik.append(self.titik[i][j] + int(self.arah_value.get()))
+                titik.append(var_titik)
+            
+            # Untuk mengaplikasikan pergeseran sesuai bentuk
+            if(self.CLICKED_BUTTON == "oval"):
+                self.use_oval(titik)
+            elif(self.CLICKED_BUTTON == "kotak"):
+                self.use_kotak(titik)    
+            elif(self.CLICKED_BUTTON == "segitiga"):    
+                self.use_segitiga(titik)
+        
         elif(self.arah_button.get() == "Kanan"):
-            self.use_oval(np.subtract(self.titik,self.arah_value.get())) 
+            titik = []
+            
+            for i in range(0, len(self.titik)):
+                var_titik =[]
+                for j in range(0,len(self.titik[i])):
+                    if j%2 == 0:
+                        var_titik.append(self.titik[i][j] + int(self.arah_value.get()))
+                    else:
+                        var_titik.append(self.titik[i][j])
+                titik.append(var_titik)
+            
+            # Untuk mengaplikasikan pergeseran sesuai bentuk
+            if(self.CLICKED_BUTTON == "oval"):
+                self.use_oval(titik)
+            elif(self.CLICKED_BUTTON == "kotak"):
+                self.use_kotak(titik)    
+            elif(self.CLICKED_BUTTON == "segitiga"):    
+                self.use_segitiga(titik) 
+        
         elif(self.arah_button.get() == "Kanan Bawah"):
-            self.use_oval(np.subtract(self.titik,self.arah_value.get()))
+            titik =[]
+            for i in range(0,len(self.titik)):
+                var_titik = []
+                for j in range(0,len(self.titik[i])):
+                    var_titik.append(self.titik[i][j] + int(self.arah_value.get()))
+
+                titik.append(var_titik)
+            
+            if(self.CLICKED_BUTTON == "oval"):
+                self.use_oval(titik)
+            elif(self.CLICKED_BUTTON == "kotak"):
+                self.use_kotak(titik)    
+            elif(self.CLICKED_BUTTON == "segitiga"):    
+                self.use_segitiga(titik)
+        
         elif(self.arah_button.get() == "Bawah"):
-            self.use_oval(np.subtract(self.titik,self.arah_value.get()))
+            titik = []
+            
+            for i in range(0, len(self.titik)):
+                var_titik =[]
+                for j in range(0,len(self.titik[i])):
+                    if j%2 == 1:
+                        var_titik.append(self.titik[i][j] + int(self.arah_value.get()))
+                    else:
+                        var_titik.append(self.titik[i][j])
+                titik.append(var_titik)
+            
+            # Untuk mengaplikasikan pergeseran sesuai bentuk
+            if(self.CLICKED_BUTTON == "oval"):
+                self.use_oval(titik)
+            elif(self.CLICKED_BUTTON == "kotak"):
+                self.use_kotak(titik)    
+            elif(self.CLICKED_BUTTON == "segitiga"):    
+                self.use_segitiga(titik)
+        
         elif(self.arah_button.get() == "Kiri Bawah"):
-            self.use_oval(np.subtract(self.titik,self.arah_value.get()))
+            titik = []
+            for i in range(0, len(self.titik)):
+                var_titik =[]
+                for j in range(0,len(self.titik[i])):
+                    if j%2 == 1:
+                        var_titik.append(self.titik[i][j] + int(self.arah_value.get()))
+                    elif j%2 == 0:
+                        var_titik.append(self.titik[i][j] - int(self.arah_value.get()))
+                titik.append(var_titik)
+            
+            # Untuk mengaplikasikan pergeseran sesuai bentuk
+            if(self.CLICKED_BUTTON == "oval"):
+                self.use_oval(titik)
+            elif(self.CLICKED_BUTTON == "kotak"):
+                self.use_kotak(titik)    
+            elif(self.CLICKED_BUTTON == "segitiga"):    
+                self.use_segitiga(titik)
         
         self.print_titik()
 
 # OTW Fungsi :
 # Perubahan tebal shape
 # Rotasi shape
+    def rotasi(self):
+        sudut = math.radians(int(self.rotasi_value.get()))
+        cos_val = math.cos(sudut)
+        sin_val = math.sin(sudut)
+        cx ,cy = self.center
+        titik = []
+
+        for x_lama , y_lama in self.titik:
+            x_lama -= cx
+            y_lama -= cy
+            x_baru = x_lama * cos_val - y_lama * sin_val
+            y_baru = x_lama * sin_val + y_lama * cos_val
+            titik.append([x_baru + cx, y_baru + cy])
+        
+        if(self.CLICKED_BUTTON == "oval"):
+            self.use_oval(titik)
+        elif(self.CLICKED_BUTTON == "kotak"):
+            self.use_kotak(titik)    
+        elif(self.CLICKED_BUTTON == "segitiga"):    
+            self.use_segitiga(titik)
 # Scaling shape
 
 
